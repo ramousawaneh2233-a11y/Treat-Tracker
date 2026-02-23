b02b1dcfc8e8cbc0509e99d4a293ab78e5475865
ramousawaneh2233-a11y <ramousawaneh2233@gmail.com>
2026-02-23 11:41:09 +0000

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -4,6 +4,8 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
