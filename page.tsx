85f3c2aa89bf0cf70cc75533d2522f2e4db12d57
ramousawaneh2233-a11y <ramousawaneh2233@gmail.com>
2026-02-23 00:28:13 +0000

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Welcome to TreatTrack</h1>
+      <p className="mt-4 text-lg">Your SaaS is officially running!</p>
+    </div>
+  );
+}
