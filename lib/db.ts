a7325de83186fefed3ebab466b26c2ba64046ed8
ramousawaneh2233-a11y <ramousawaneh2233@gmail.com>
2026-02-23 18:21:07 +0000

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient({
+    // This is where the URL goes now in Prisma 7
+    datasourceUrl: process.env.DATABASE_URL, 
+  })
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
