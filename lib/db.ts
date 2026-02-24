794902b4bda17077455b1a82020b5f058fcaf2c2
ramousawaneh2233-a11y <ramousawaneh2233@gmail.com>
2026-02-24 07:54:59 +0000

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -6,3 +6,8 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 export const db = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+model User {
+  id    String  @id @default(cuid())
+  email String  @unique
+  name  String?
+}
