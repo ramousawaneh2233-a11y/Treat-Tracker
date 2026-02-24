677fca92e321ddb9716d5c4c86dff1037ee5d34d
ramousawaneh2233-a11y <ramousawaneh2233@gmail.com>
2026-02-24 09:00:36 +0000

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -3,13 +3,8 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
 const prismaClientSingleton = () => {
-  // 1. Create a connection pool to Supabase
   const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
-  
-  // 2. Wrap it in the Prisma Driver Adapter
   const adapter = new PrismaPg(pool);
-  
-  // 3. Pass the adapter to the Client (This fixes the "InitializationError")
   return new PrismaClient({ adapter });
 };
 
