aecc34670b3d4ba99c4ffc85694c12ff30aabd15
ramousawaneh2233-a11y <ramousawaneh2233@gmail.com>
2026-02-24 12:59:41 +0000

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,13 @@
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import pg from 'pg';
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
-const prismaClientSingleton = () => {
-  const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
-  const adapter = new PrismaPg(pool);
-  return new PrismaClient({ adapter });
-};
+const connectionString = `${process.env.DATABASE_URL}`
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof prismaClientSingleton> | undefined;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const db = globalForPrisma.prisma ?? prismaClientSingleton();
+export const db = globalForPrisma.prisma || new PrismaClient({ adapter })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
